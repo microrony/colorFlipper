e1bc475c184330c4f5f119fd37bc910c4cf52ecb
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-25 14:46:10 +0600

    completed the first part

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 // variables
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+const body = document.body;
+
+// function
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * colors.length);
+}
+
+const flipColor = () => {
+  // get randoem number 0 - 3
+  const randomNumber = getRandomNumber();
+  body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+}
+// event 
+btn.addEventListener('click', flipColor);
